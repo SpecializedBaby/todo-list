e0ce5da613c275bc58e5f495c544ffdedba17004
Dede <specialized8393@gmail.com>
2024-06-04 02:03:40 +0100

    feat: add all pathes for Task and Tag views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
 
+from task_manager.views import (
+    TaskListView,
+    ToggleTaskStatusView,
+    TagListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
+
 
 urlpatterns = [
-
+    path("", TaskListView.as_view(), name="task-list"),
+    path("toggle/<int:pk>", ToggleTaskStatusView.as_view(), name="toggle-task-status"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "task_manager"

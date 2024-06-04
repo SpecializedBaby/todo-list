30ea36756fe3e654a7f1b8f5eb4a7d245cb92612
Dede <specialized8393@gmail.com>
2024-06-04 02:05:08 +0100

    feat: add CRUD & toggle views for Task and Tag

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    ordering = ["is_done", "-created_at"]
+    # template_name = "task_manager/task_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class ToggleTaskStatusView(generic.View):
+
+    def get(self, request, pk):
+        task = Task.objects.get(id=pk)
+
+        if task.is_done:
+            task.is_done = False
+        else:
+            task.is_done = True
+        task.save()
+
+        return redirect("task_manager:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("task_manager:tag-list")

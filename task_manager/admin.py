1a7a5b5acbe71c298ec1508eb590e338b0f352d6
Dede <specialized8393@gmail.com>
2024-06-04 10:23:32 +0100

    фÐapply black modul and add empty line

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -4,13 +4,13 @@ from .models import Task, Tag
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('content', 'created_at', 'deadline', 'is_done')
-    list_filter = ('is_done', 'created_at', 'deadline')
-    search_fields = ('content',)
-    autocomplete_fields = ('tags',)
+    list_display = ("content", "created_at", "deadline", "is_done")
+    list_filter = ("is_done", "created_at", "deadline")
+    search_fields = ("content",)
+    autocomplete_fields = ("tags",)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    search_fields = ("name",)
